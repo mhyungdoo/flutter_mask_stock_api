e0e8303be43f48fa3c72eec42a2cfaa7764c2fd6
mhyungdoo <mhyungdoo@gmail.com>
2020-08-20 13:06:10 +0900

    mask app start_JSON print

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_mask_stock_api/model/store.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
@@ -30,25 +31,40 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  final stores = List<Store>();
+
   Future fetch() async {
-    var url = 'https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json?lat=37.637902&lng=126.919325&m=5000';
+    var url = 'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json';
+
     var response = await http.get(url);
-    print('Response status: ${response.statusCode}');
- //  print('Response body: ${response.body}');  // 한글 깨짐 발생
-  //  print('Response body: ${utf8.decode(response.bodyBytes)}');  //한글 깨지지 않도록 변경
-    //다시 JSON 형태로 변경
-    print('Response body: ${jsonDecode(utf8.decode(response.bodyBytes))}');
+    final jsonResult = jsonDecode(utf8.decode(response.bodyBytes));
+
+    final jsonStores = jsonResult['stores'];
+
+    stores.clear();
+    jsonStores.forEach((e){
+      stores.add(Store.fromJson(e));
+      });
+
+    //  print(jsonResult['stores']);  // JSON에서 불러오고 싶은 key 값 지정
+    //  print('Response status: ${response.statusCode}');
+    //  print('Response body: ${utf8.decode(response.bodyBytes)}');  //한글 깨지지 않도록 변경
+    //  다시 JSON 형태로 변경
+    //  print('Response body: ${jsonDecode(utf8.decode(response.bodyBytes))}');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Open API'),),
+        title: Text('마스크 재고 있는 곳 : 0 곳'),),
       body: Center(
       child: RaisedButton(
-        onPressed: fetch,
-        child: Text('test'),
+        onPressed: () async {
+          await fetch();
+          print(stores.length);
+        },
+        child: Text('테스트'),
       ),
       ),
     );
