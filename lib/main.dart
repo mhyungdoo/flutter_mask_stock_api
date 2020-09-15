dce118764457f3687c2fd5838ff74cc9daa6f84b
mhyungdoo <mhyungdoo@gmail.com>
2020-09-15 20:16:18 +0900

    Mask stock in Notebook 마무리

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {  //상태 변경할 필요가 없으므로 stateless로 변경. 상태 변경은 다른 파일로 분리
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
